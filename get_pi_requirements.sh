99bd49b42dfdf62c499e6f184729f60696feb464
Nguyễn Gia Bảo <104297969+BaoBao112233@users.noreply.github.com>
2023-12-26 11:00:57 +0700

    Update get_pi_requirements.sh

--- get_pi_requirements.sh
+++ get_pi_requirements.sh
@@ -2,14 +2,15 @@
 
 # Get packages required for OpenCV 34
 
-sudo apt-get -y install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt-get -y install libjpeg-dev libtiff5-dev libjasper-dev libpng-dev
 sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 sudo apt-get -y install libxvidcore-dev libx264-dev
-sudo apt-get -y install qt4-dev-tools 
+sudo apt-get -y install qt5base-tools 
 sudo apt-get -y install libatlas-base-dev
 sudo apt-get -y install libgeos-dev
 
 # Need to get an older version of OpenCV because version 4 has errors   3
+pip3 install numpy==1.20.1
 pip3 install opencv-python==3.4.11.41     
 pip3 install opencv-contrib-python==3.4.11.45
 pip3 install shapely
